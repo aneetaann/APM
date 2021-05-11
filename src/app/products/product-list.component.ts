c7b2888e596f634cb8cdc2d99ece00221e575b19
Aneeta <aneetajohn98@gmail.com>
2021-05-12 01:15:22 +0530

    new commit http

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 @Component({
@@ -7,13 +8,15 @@ import { ProductService } from './product.service';
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
     //properties
     pageTitle:string ='Product List';
     imageWidth: number= 50;
     imageMargin: number= 2;
     showImage: boolean= false;
-    
+    errorMessage: string = '';
+    sub!: Subscription;
+
     private _listFilter: string = '';
     get listFilter(): string{
         return this._listFilter;
@@ -41,10 +44,19 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void{
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+        
     }
 
+    ngOnDestroy(){
+        this.sub.unsubscribe();
+    }
     onRatingClicked(message: string): void{
         this.pageTitle='Product List: ' + message;
     }
