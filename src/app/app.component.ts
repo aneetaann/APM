1e195f7591192dc8ed809fcec31f4632247604aa
Aneeta <aneetajohn98@gmail.com>
2021-05-14 23:35:00 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, Directive } from "@angular/core";
 @Component({ 
   selector: "pm-root",
   template: 
-  `<nav class = 'navbar navbar-expand navbar-light bg-light>
+  `<nav class = navbar navbar-expand navbar-light bg-light>
       <a class = 'navbar-brand'>{{pageTitle}}</a>
       <ul class = 'nav nav-pills'>
         <li><a class = 'nav-link' [routerLink] = "['/welcome']"> Home </a></li>
